3ac2e2c8fcc9be842fb3c9d040555d80b6075459
lh99j <grand2181@gmail.com>
2023-11-09 17:27:49 +0900

    test(DDayDiscountTest): 할인 금액 계산 기능 테스트 작성

---
+++ src/test/kotlin/christmas/model/DDayDiscountTest.kt
@@ -0,0 +1,19 @@
+package christmas.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+
+class DDayDiscountTest {
+    private val dDayDiscount = DDayDiscount()
+
+    @ParameterizedTest
+    @DisplayName("입력된 날짜에 해당하는 할인 금액을 가져온다.")
+    @CsvSource(value = ["1:1000", "3:1200", "21:3000", "25:3400", "26:0"])
+    fun getPriceTest(input: Int, expected: Int) {
+        val discountPrice = dDayDiscount.getPrice(input)
+        assertThat(discountPrice).isEqualTo(expected)
+    }
+}
