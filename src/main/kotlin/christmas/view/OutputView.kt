bdceadf257f3534940f46f0f180e4374a8c64847
lh99j <grand2181@gmail.com>
2023-11-10 13:20:03 +0900

    feat(OutputView): 인사말 출력 메세지 기능 구현

---
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -0,0 +1,11 @@
+package christmas.view
+
+class OutputView {
+    fun printGreetings() {
+        println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
+    }
+
+    fun printMenu() {
+        println("<주문 메뉴>")
+    }
+}
