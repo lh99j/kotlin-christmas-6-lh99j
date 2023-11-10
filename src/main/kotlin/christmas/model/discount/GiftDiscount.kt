5eb25dfe421d34d5e8b8fb298e5624d968e9a1ac
lh99j <grand2181@gmail.com>
2023-11-10 17:46:54 +0900

    feat(GiftDiscount): 증정 할인 계산 기능 구현

--- src/main/kotlin/christmas/model/discount/GiftDiscount.kt
+++ src/main/kotlin/christmas/model/discount/GiftDiscount.kt
@@ -3,9 +3,7 @@ package christmas.model.discount
 import christmas.model.Order
 
 class GiftDiscount(private val order: Order) : Discount {
-    override fun getPrice(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getPrice(): Int = 25000
 
     override fun checkTarget(): Boolean = order.getTotalPrice() > 120000
 }
