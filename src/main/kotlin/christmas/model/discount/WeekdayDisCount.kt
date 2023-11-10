4c6ad4e07c4abab303cec1c6e7739425c7cfcd97
lh99j <grand2181@gmail.com>
2023-11-10 17:45:59 +0900

    feat(WeekdayDiscount): 평일 할인 가격 계산 기능 구현

--- src/main/kotlin/christmas/model/discount/WeekdayDisCount.kt
+++ src/main/kotlin/christmas/model/discount/WeekdayDisCount.kt
@@ -5,7 +5,8 @@ import christmas.model.Order
 
 class WeekdayDisCount(private val order: Order, private val day: Int) : Discount {
     override fun getPrice(): Int {
-        TODO("Not yet implemented")
+        val orderMenu = order.getMenu()
+        return orderMenu[2].size * 2023
     }
 
     override fun checkTarget(): Boolean {
