51a1fda48c4d8898ed9e156e4ebbda5ca9a6a385
lh99j <grand2181@gmail.com>
2023-11-10 18:40:49 +0900

    refactor: 클래스 삭제

--- src/main/kotlin/christmas/model/discount/WeekdayDiscount.kt
+++ src/main/kotlin/christmas/model/discount/WeekdayDiscount.kt
@@ -3,10 +3,11 @@ package christmas.model.discount
 import christmas.model.Calendar
 import christmas.model.Order
 
-class WeekdayDiscount(private val order: Order, private val day: Int) : Discount {
+class WeekdayDisCount(private val order: Order, private val day: Int) : Discount {
     override fun getPrice(): Int {
         val orderMenu = order.getMenu()
-        return orderMenu[2].size * 2023
+        val totalCount = orderMenu[2].entries.sumOf { it.value }
+        return totalCount * 2023
     }
 
     override fun checkTarget(): Boolean {
