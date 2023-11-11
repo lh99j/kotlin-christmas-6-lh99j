83c7d1df79da5fcdfca542ba1b4019419a74fe4b
lh99j <grand2181@gmail.com>
2023-11-11 13:55:05 +0900

    feat(Benefits): 증정 할인을 뺀 총 할인 금액 가져오는 기능 구현

--- src/main/kotlin/christmas/model/Benefits.kt
+++ src/main/kotlin/christmas/model/Benefits.kt
@@ -1,5 +1,7 @@
 package christmas.model
 
+import christmas.util.Constants.GIFT_DISCOUNT
+
 class Benefits {
     private var _history = mutableMapOf<String, Int>()
     val history: Map<String, Int>
@@ -9,5 +11,9 @@ class Benefits {
         _history[type] = _history.getOrDefault(type, 0) + price
     }
 
-    fun getTotalBenefit(): Int = _history.entries.sumOf { it.value }
+    fun getTotalBenefit(): Int = _history.values.sum()
+
+    fun getTotalDiscount(): Int =
+        _history.values.sum() - _history.getOrDefault(GIFT_DISCOUNT, 0)
+
 }
