c6d0cfac6674ef944317fcfb78239dff20bb6848
lh99j <grand2181@gmail.com>
2023-11-12 15:50:31 +0900

    refactor(util): 가독성을 위한 변수 추가 및 공백 추가

--- src/main/kotlin/christmas/util/Constants.kt
+++ src/main/kotlin/christmas/util/Constants.kt
@@ -5,6 +5,7 @@ object Constants {
     const val MAIN_FOOD_INDEX = 1
     const val DESSERT_INDEX = 2
     const val DRINK_INDEX = 3
+
     const val DISCOUNT_PRICE = 2_023
     const val SPECIAL_DISCOUNT_PRICE = 1_000
     const val GIFT_DISCOUNT_PRICE = 25_000
@@ -12,16 +13,19 @@ object Constants {
     const val START_DISCOUNT_PRICE = 1_000
     const val DISCOUNT_MIN_PRICE = 10_000
     const val INCREASE_PRICE = 100
-    const val EVENT_END_DAY = 26
+
     const val DDAY_DISCOUNT = "크리스마스 디데이 할인"
     const val WEEKDAY_DISCOUNT = "평일 할인"
     const val WEEKEND_DISCOUNT = "주말 할인"
     const val SPECIAL_DISCOUNT = "특별 할인"
     const val GIFT_DISCOUNT = "증정 이벤트"
+
     const val MIN_DATE = 1
     const val MAX_DATE = 31
+
     const val MAX_ORDER_COUNT = 20
     const val SPLIT_SIZE = 2
     const val COUNT_INDEX = 1
     const val FOOD_CATEGORY_SIZE = 4
+    const val EVENT_END_DAY = 26
 }
--- src/main/kotlin/christmas/util/Validator.kt
+++ src/main/kotlin/christmas/util/Validator.kt
@@ -10,28 +10,31 @@ import christmas.util.Constants.MIN_DATE
 import christmas.util.Constants.SPLIT_SIZE
 
 object Validator {
+    private const val INVALID_DATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."
+    private const val INVALID_ORDER = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."
+
     fun validateNotNull(input: String) =
-        require(input.trim().isNotEmpty()) { "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." }
+        require(input.trim().isNotEmpty()) { INVALID_DATE }
 
     fun validateInteger(input: String) =
-        require(input.toIntOrNull() != null) { "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." }
+        require(input.toIntOrNull() != null) { INVALID_DATE }
 
     fun validateDateRange(input: String) =
-        require(input.toInt() in MIN_DATE..MAX_DATE) { "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." }
+        require(input.toInt() in MIN_DATE..MAX_DATE) { INVALID_DATE }
 
     fun validateOrderNotNull(input: List<String>) =
-        require(input.all { it.trim().isNotEmpty() }) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+        require(input.all { it.trim().isNotEmpty() }) { INVALID_ORDER }
 
     fun validateOrderForm(input: List<String>) {
         input.forEach {
             val splitInput = it.split("-")
-            require(splitInput.size == SPLIT_SIZE) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
-            require(splitInput[COUNT_INDEX].toIntOrNull() != null) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+            require(splitInput.size == SPLIT_SIZE) { INVALID_ORDER }
+            require(splitInput[COUNT_INDEX].toIntOrNull() != null) { INVALID_ORDER }
         }
     }
 
     fun validateOrderCount(input: List<List<OrderForm>>) {
-        require(input.flatten().sumOf { it.count } <= MAX_ORDER_COUNT) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+        require(input.flatten().sumOf { it.count } <= MAX_ORDER_COUNT) { INVALID_ORDER }
     }
 
     fun validateMenuName(foods: List<List<Menu>>, input: String) {
@@ -40,16 +43,16 @@ object Validator {
                 menu.name == input
             }
         }
-        require(validation) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+        require(validation) { INVALID_ORDER }
     }
 
     fun validateNotOnlyDrink(menu: List<List<OrderForm>>) {
-        require(menu.flatten().size != menu[DRINK_INDEX].size) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+        require(menu.flatten().size != menu[DRINK_INDEX].size) { INVALID_ORDER }
     }
 
     fun validateUniqueOrder(menu: List<List<OrderForm>>) {
         require(
             menu.flatten().distinctBy { it.menu.name }.size == menu.flatten().size
-        ) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+        ) { INVALID_ORDER }
     }
 }
